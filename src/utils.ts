b2060fe96a64c1c8d83e45cafff1a008ae13debe
Azalea Gui <me@hydev.org>
2023-02-14 14:30:59 -0500

    [O] Better sizeFmt

--- src/utils.ts
+++ src/utils.ts
@@ -5,8 +5,10 @@
  * @returns Human readable size in string
  */
 export function sizeFmt(size: number): string {
-    var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
-    return (size / Math.pow(1024, i)).toFixed(1) + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+    const i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+    size = size / Math.pow(1024, i)
+    const fmt = size >= 100 ? size.toFixed(0) : size.toFixed(1)
+    return fmt + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
 }
 
 export function clamp(num: number, min: number, max: number) {
