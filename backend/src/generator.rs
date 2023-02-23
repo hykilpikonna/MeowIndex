8d7ba396b2515dfa0c76a8c25f8122319fa25897
Azalea Gui <me@hydev.org>
2023-02-23 11:52:45 -0500

    [+] make Generator class

--- backend/src/generator.rs
+++ backend/src/generator.rs
@@ -1,6 +1,11 @@
 use std::os::unix::fs::MetadataExt;
 use pathdiff::diff_paths;
 use std::path::{PathBuf};
+use std::fs::{File, Metadata};
+use std::fs::DirEntry;
+use std::io::{BufReader};
+use xdg_mime::{SharedMimeInfo};
+use anyhow::{Result};
 use serde::{de, Deserialize, ser, Serialize};
 
 const DOT_PATH: &str = ".meow_index";
@@ -11,3 +16,9 @@ pub struct ReturnPath {
     pub(crate) file_type: String,
     pub(crate) mtime: i64
 }
+
+pub struct Generator {
+    pub(crate) mime_db: SharedMimeInfo,
+    pub(crate) base: PathBuf,
+}
+
