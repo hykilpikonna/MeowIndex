c32055f213a4417d1f14fcccd59f78987ad51513
Azalea Gui <me@hydev.org>
2023-02-24 12:02:39 -0500

    [T] Test list_video_files

--- backend/src/test.rs
+++ backend/src/test.rs
@@ -5,6 +5,7 @@ mod thumbnailer;
 mod encoder;
 
 use std::path::{Path, PathBuf};
+use std::process::exit;
 use generator::*;
 use crate::encoder::Encoders;
 use crate::thumbnailer::{Thumbnailer, Thumbnailers};
@@ -15,22 +16,28 @@ extern crate pretty_env_logger;
 fn main() {
     pretty_env_logger::init();
 
-    // let gen = Generator::new("/data".into()).unwrap();
-    // 
+    let gen = Generator::new("/data".into()).unwrap();
+    //
     // let path: PathBuf = "/data/Anime/1977 Star Wars Collection/01 Star Wars Episode I The Phantom Menace - George Lucas 1999 Eng Subs 720p [H264-mp4].mp4".into();
     // let mime = gen.get_mime(&path)
     //     .expect("Panic");
     // info!("mime {mime}");
-    // 
+    //
     // let thumbnailer_path = "/usr/share/thumbnailers/totem.thumbnailer";
     // let thumbnailer = Thumbnailer::load(Path::new(thumbnailer_path)).unwrap();
     // info!("thumb {:?}", thumbnailer);
     // info!("check {:?}", thumbnailer.check("audio/x-mp3"));
     // thumbnailer.gen(path.to_str().unwrap(), "/tmp/test.png", 256).expect("Generation failed");
-    // 
+    // gen.get_thumb(&path).expect("Get thumb failed");
+    //
     // let ts = Thumbnailers::load_all().unwrap();
-    // info!("Video thumbnailer: {:?}", ts.find("audio/x-mp3"))
-    
-    let encoders = Encoders::load().unwrap();
-    info!("Encoders {:?}", encoders);
+    // info!("Video thumbnailer: {:?}", ts.find("audio/x-mp3"));
+    //
+    // let encoders = Encoders::load().unwrap();
+    // info!("Encoders {:?}", encoders);
+
+
+    let videos = gen.list_video_files(&PathBuf::from("/data/Anime"));
+    videos.iter().for_each(|x| println!("Video found: {}", x.display()));
+    println!("Length: {}", videos.len());
 }
