ee67670a897acce9e1e8e2dbd9c17b53652e9982
Azalea Gui <me@hydev.org>
2023-02-24 12:22:35 -0500

    [+] Encoder::exec_all

--- backend/src/encoder.rs
+++ backend/src/encoder.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 use std::process::{Command, Output};
 use serde::{Deserialize};
 use anyhow::{Context, Result};
+use tempdir::TempDir;
 use crate::utils::run_cmd;
 
 #[derive(Deserialize, Debug)]
@@ -31,6 +32,28 @@ impl Encoders {
         let file = env::var("MEOW_ENCODING_FILE").unwrap_or("encoding.toml".to_string());
         let content = fs::read_to_string(file)?;
         Ok(toml::from_str(&*content)?)
+    }
+
+    pub fn exec_all(&self, orig: &str, out: &Path) -> Result<()> {
+        for enc in self.encoders {
+            let enc_out = out.with_extension(enc.suffix);
+            // Skip if encoded video already exists
+            if enc_out.exists() { continue }
+
+            // Create tmp (this is to prevent partially completed encoding results being detected as completed)
+            let tmp_dir = TempDir::new("meow_encoder_tmp")?;
+            let tmp_out = tmp_dir.path().join(out.file_name().context("No file name")?);
+
+            // Convert to tmp
+            enc.execute(orig, tmp_out.to_str().context("Call to path.to_str failed")?)?;
+
+            // Copy results
+            fs::copy(tmp_out, enc_out)?;
+
+            // Cleanup tmp
+            tmp_dir.close()?;
+        }
+        Ok(())
     }
 }
 
